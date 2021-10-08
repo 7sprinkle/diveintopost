2f1d0b5297176b92d8a29f699be73a08d94d67d1
septem <qitengyuxi@gmail.com>
2021-10-09 02:33:37 +0900

    権限移動#65

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -15,7 +15,12 @@ class TeamsController < ApplicationController
     @team = Team.new
   end
 
-  def edit; end
+  def edit
+    if current_user != @team.owner
+      flash.now[:error] = I18n.t('cannot_edit_not_admin')
+      render :show
+    end
+  end
 
   def create
     @team = Team.new(team_params)
@@ -48,6 +53,15 @@ class TeamsController < ApplicationController
   end
 
   private
+
+  def owner_change
+    @team = current_user.keep_team
+    @team.update(owner_id: params[:owner_id])
+    @user = User.find(@team.owner_id)
+    TeamMailer.team_mail(@user).deliver
+    redirect_to @team, notice: I18n.t('views.messages.owner_change')
+  end
+
 
   def set_team
     @team = Team.friendly.find(params[:id])
